2220c4737502b99715c86bbb0457ba5106b3f3a2
Hyunwook Shin <46451881+shinhyu3@users.noreply.github.com>
2019-08-12 21:51:27 -0700

    Update UrlValidatorTest.java

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -1,1 +1,7 @@
+public void testUnit2() {
+    	String url = ""
+    	UrlValidator validTest = new UrlValidator();
+    	//url is invalid and therefore the message will be printed.
+    	assertFalse("url is valid.", validTest.isValid(url); 
+    }
 
